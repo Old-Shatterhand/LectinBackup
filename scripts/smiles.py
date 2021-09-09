5a9f187270a956f7d30f706931f40cc6cd3c73c8
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2021-09-09 16:19:09 +0200

    All smiles computed

--- scripts/smiles.py
+++ scripts/smiles.py
@@ -1,25 +1,28 @@
 import pandas as pd
 from rdkit import Chem
-# from rdkit.Chem.rdmolfiles import MolFromPDBFile, MolToSmiles
+import os
 
 
 def main():
     df = pd.read_csv("glycans/ids.csv", dtype=str)
     smiles = []
     for index, row in df.iterrows():
+        if not os.path.exists("./glycans/" + row["Code"] + ".pdb"):
+            smiles.append("")
+            continue
+
         mol = Chem.MolFromPDBFile("./glycans/" + row["Code"] + ".pdb")
         if "one" in str(type(mol)):
             smiles.append("")
             continue
-        print(Chem.MolToSmiles(mol))
-        # Chem.AssignAtomChiralTagsFromStructure(mol)
-        Chem.AssignChiralTypesFromBondDirs(mol)
-        # smiles.append(Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True))
-        print(Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True), end="\n\n")
-        if index == 30:
-            return
+
+        # print(Chem.MolToSmiles(mol))
+        Chem.AssignAtomChiralTagsFromStructure(mol)
+        # Chem.AssignChiralTypesFromBondDirs(mol)
+        smiles.append(Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True))
+        # print(Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True), end="\n\n")
     df["Smiles"] = smiles
-    df.to_csv("glycans.csv", index=False)
+    df.to_csv("smiles.csv", index=False)
 
 
 if __name__ == '__main__':
